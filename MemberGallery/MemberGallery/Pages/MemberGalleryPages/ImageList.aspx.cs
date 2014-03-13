41424c18f3b336b301f8ec41f74ed35b4414536d
Grenmyr <dg222cs@student.lnu.se>
2014-03-13 12:46:41 +0100

    git commit

--- MemberGallery/MemberGallery/Pages/MemberGalleryPages/ImageList.aspx.cs
+++ MemberGallery/MemberGallery/Pages/MemberGalleryPages/ImageList.aspx.cs
@@ -77,7 +77,7 @@ namespace MemberGallery.Pages.MemberGalleryPages
                         // Message saved in Extension method.
                         Page.SetTempData("Confirmation", String.Format("Bilden {0} har sparats", image.ImgName));
                         // TODO: Want to redirect to uploaded pages category.
-                        Response.RedirectToRoute("ImageList", new object { CategoryID = cat.CategoryID });
+                        Response.RedirectToRoute("ImageList", new  { CategoryID = cat.CategoryID });
                         Context.ApplicationInstance.CompleteRequest();
                     }
                     catch (Exception)

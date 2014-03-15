d1b548aad575bb571b940bf2d1e074c8e62faa31
Grenmyr <dg222cs@student.lnu.se>
2014-03-15 13:04:27 +0100

    Skiter i det där.

--- MemberGallery/MemberGallery/App_Infrastructure/RouteConfig.cs
+++ MemberGallery/MemberGallery/App_Infrastructure/RouteConfig.cs
@@ -17,7 +17,8 @@ namespace MemberGallery.App_Infrastructure
     
             route.MapPageRoute("CategoriesList", "", "~/Pages/MemberGalleryPages/CategoryList.aspx");
             route.MapPageRoute("Default", "", "~/Pages/Shared/Categories.ascx");
-            route.MapPageRoute("Categories", "Kategorier", "~/Pages/MemberGalleryPages/Categories.ascx");
+            //route.MapPageRoute("Categories", "Kategorier", "~/Pages/MemberGalleryPages/Categories.ascx");
+            route.MapPageRoute("UpLoad", "Ladda UPP", "~/Pages/MemberGalleryPages/Shared/Categories.ascx");
             // Sending using CategoryID and Image in this 2 pages browserfield.
             route.MapPageRoute("ImageList", "BildKategori/{CategoryID}", "~/Pages/MemberGalleryPages/ImageList.aspx");
             route.MapPageRoute("Image", "BildKategori/{CategoryID}/Bild/{ImageID}", "~/Pages/MemberGalleryPages/Image.aspx");
--- MemberGallery/MemberGallery/Pages/Shared/Site.Master.designer.cs
+++ MemberGallery/MemberGallery/Pages/Shared/Site.Master.designer.cs
@@ -58,15 +58,6 @@ namespace MemberGallery.Pages.Shared {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder;
         
         /// <summary>
-        /// Upload control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::MemberGallery.Pages.Shared.Upload Upload;
-        
-        /// <summary>
         /// Categories control.
         /// </summary>
         /// <remarks>
